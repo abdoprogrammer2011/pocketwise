259407e1afaa37e65a0d166c4886273e2cbc931c
abdoprogrammer2011 <abdelrahman.ellathy2492011@gmail.com>
2026-08-20 09:40:13 +0300

    initial upload

--- src/firebaseConfig.js
+++
@@ -1,6 +0,0 @@
-/**
- * Convenience re-export so firebaseConfig.js lives at src/ as requested.
- * Put your Firebase keys in src/firebase/config.js
- */
-export { auth, db, analytics } from './firebase/config';
-export { default } from './firebase/config';
